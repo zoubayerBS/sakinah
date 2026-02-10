a2cf16d19422feece0dac1dcce637071c9e83346
ZOUBAYER BENSAID <zoubayerbensaid@ZOUBAYERs-MacBook-Air.local>
2026-02-10 08:50:32 +0100

    chore: update service worker revision and remove unused assets

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-30ad1a2e'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.udmh35qf1f8"
+    "revision": "0.ivg2en2mo88"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
